fc45a39c118ac1f265c774bcc47747473da31b4f
Kazuma SATO <ranceworks@gmail.com>
2015-12-19 17:12:32 +0900

    add calcSimilarity

--- src/main/scala/com/ranceworks/nanao/vsm/MAnalyzer.scala
+++ src/main/scala/com/ranceworks/nanao/vsm/MAnalyzer.scala
@@ -22,7 +22,10 @@ object MAnalyzer {
 
     val tft = createTf(tokens.toList)
     val tfh = createTf(hoges.toList)
-    println (calcIdf(List(tft, tfh)))
+    val idf = calcIdf(List(tft, tfh))
+
+    println (calcTfIdf(tft, idf))
+    println (calcTfIdf(tfh, idf))
   }
 
 
@@ -34,7 +37,7 @@ object MAnalyzer {
       }
       b
     }
-    Tf((scala.collection.mutable.Map[String, Int]() /: tokens)(fn))
+    Tf((scala.collection.mutable.Map[String, Int]() /: tokens)(fn).toMap)
   }
 
   private def calcIdf(tfs: List[Tf]): Idf = {
@@ -53,16 +56,27 @@ object MAnalyzer {
       b
     }
     val countMap = (scala.collection.mutable.Map[String, Int]() /: freqs)(fn)
-    Idf(countMap.map((b: (String, Int)) => (b._1, math.log((d + 1).toDouble/(b._2 + 1).toDouble))))
+    Idf(countMap.map((b: (String, Int)) => (b._1, math.log((d + 1).toDouble/(b._2 + 1).toDouble))).toMap)
   }
 
   private def calcTfIdf(tf: Tf, idf: Idf): TfIdf = {
-    TfIdf(tf.terms.map(a => (a._1, a._2 * idf.terms(a._1))).toMap)
+    
+    TfIdf(idf.terms.map(a => (a._1, tf.terms.getOrElse(a._1, 0) * a._2)))
+  }
+  private def calcSimilarity(left: TfIdf, right: TfIdf) = {
+    def norm(tfidf: TfIdf) = math.sqrt((0.0 /: tfidf.terms.values)((a: Double, b: Double) => a + math.pow(b, 2)))
+
+    left.terms.map(f=> right.terms.getOrElse(f._1, 0))
+
+    val decimel = (0.0 /: left.terms)((a: Double, b: (String, Double)) => a + (b._2 * right.terms.getOrElse(b._1, 0.0)))
+
+    decimel / (norm(left) * norm(right))
   }
 }
 
-case class Tf(terms: scala.collection.mutable.Map[String, Int])
+case class Tf(terms: Map[String, Int])
 
-case class Idf(terms: scala.collection.mutable.Map[String, Double])
+case class Idf(terms: Map[String, Double])
 
-case class TfIdf(terms: Map[String, Double])
+case class TfIdf(terms: Map[String, Double]) {
+}
